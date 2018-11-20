79260a1fefe088391fd21dac6c6dabac5d5da81b
Ofir Germansky <ofirg.pub@gmail.com>
2018-11-20 14:14:52 -0600

    adding project

---
+++ src/main/java/com/exercise/anddigital/AndDigitalApplication.java
@@ -0,0 +1,12 @@
+package com.exercise.anddigital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AndDigitalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AndDigitalApplication.class, args);
+    }
+}
---
+++ src/test/java/com/exercise/anddigital/AndDigitalApplicationTests.java
@@ -0,0 +1,16 @@
+package com.exercise.anddigital;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AndDigitalApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
