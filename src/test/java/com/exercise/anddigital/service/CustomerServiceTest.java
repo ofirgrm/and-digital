fed23cc5b3a5c9dae214115dba7181c70e595155
Ofir Germansky <ofirg.pub@gmail.com>
2018-11-20 15:05:28 -0600

    adding 2 tests for get customer phones

--- src/test/java/com/exercise/anddigital/service/CustomerServiceTest.java
+++ src/test/java/com/exercise/anddigital/service/CustomerServiceTest.java
@@ -9,6 +9,7 @@ import org.junit.Test;
 import java.util.List;
 
 import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertThat;
 
 public class CustomerServiceTest {
@@ -33,12 +34,21 @@ public class CustomerServiceTest {
     public void getAllCustomersPhoneNumbers() {
         List<String> phones = this.customerService.getAllCustomersPhoneNumbers();
 
+        assertNotNull(phones);
         assertThat(phones.size(), is(6));
     }
 
     @Test
     public void getCustomerPhoneNumbers() {
+        List<String> phones = this.customerService.getCustomerPhoneNumbers(1l);
 
+        assertNotNull(phones);
+        assertThat(phones.size(), is(3));
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void getCustomerPhoneNumbersCustomerNotFound() {
+        List<String> phones = this.customerService.getCustomerPhoneNumbers(4l);
     }
 
     @Test
